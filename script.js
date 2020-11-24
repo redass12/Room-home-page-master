8291076d2f2bddd4327c3ece2903b4a9e91ef5e7
redass12 <redataraoui1@gmail.com>
2020-11-24 10:19:06 +0100

    rebuid the project fix responsivness v1

--- script.js
+++ script.js
@@ -1,62 +1,73 @@
 const img = document.querySelectorAll('#img');
-const btnNext = document.getElementById('next');
-const btnPrev = document.getElementById('prev');
+// const btnNext = document.getElementById('next');
+// const btnPrev = document.getElementById('prev');
 const sliders = document.querySelectorAll('.slider');
-let i = 0;
-
-const check = document.getElementById('check');
 
 
-const nextSlider = () => {
-    sliders[i].classList.remove('active');
-    // console.log(i);
-    if(i>=sliders.length-1){
-        i =-1;
+// let i = 0;
+
+// const check = document.getElementById('check');
+
+
+// const nextSlider = () => {
+//     sliders[i].classList.remove('active');
+//     // console.log(i);
+//     if(i>=sliders.length-1){
+//         i =-1;
+//     }
+//     sliders[i+1].classList.add('active');
+//     i++;
+
+// }
+
+// const prevSlider = () => {
+// console.log(i);
+// sliders[i].classList.remove('active');
+// if(i === 0) {
+//     i = 3;
+// }
+// sliders[i-1].classList.add('active');
+// console.log(i);
+// i--;
+// }
+
+
+// const prevNextBtn = (prevOrNext) =>{
+// if(prevOrNext === 'Next'){
+//     return nextSlider();
+// }else{
+//     return prevSlider();
+// }
+// }
+
+
+
+console.log(img);
+
+
+function displayAllImgs (x){
+    if(x.matches){
+        img.forEach((img,i)=> {
+            img.src = `./images/mobile-image-hero-${i+1}.jpg`
+        });
+
+
+    }else{
+        img.forEach((img,i)=> {
+            img.src = `./images/desktop-image-hero-${i+1}.jpg`
+        })
     }
-    sliders[i+1].classList.add('active');
-    i++;
-
+    
 }
 
-const prevSlider = () => {
-console.log(i);
-sliders[i].classList.remove('active');
-if(i === 0) {
-    i = 3;
-}
-sliders[i-1].classList.add('active');
-console.log(i);
-i--;
-}
+const x = window.matchMedia("(max-width: 420px)");
+displayAllImgs(x);
 
-
-const prevNextBtn = (prevOrNext) =>{
-if(prevOrNext === 'Next'){
-    return nextSlider();
-}else{
-    return prevSlider();
-}
-}
-
-
-
-
-function displayAllImgs (){
-    img.forEach((img,i)=> {
-        img.style.background = `url('./images/desktop-image-hero-${i+1}.jpg') no-repeat `
-        img.style.backgroundSize='cover';
-        // img.style.backgroundPosition='center';
-    })
-}
-
-
-displayAllImgs();
-
-btnNext.addEventListener('click', prevNextBtn.bind(null,'Next'));
-btnPrev.addEventListener('click',prevNextBtn.bind(null,'Prev'));
-check.addEventListener('change', () => {
-    console.log(check.checked);
-    if(check.checked) {
-        console.log('reda');
-    }
-})
+// btnNext.addEventListener('click', prevNextBtn.bind(null,'Next'));
+// btnPrev.addEventListener('click',prevNextBtn.bind(null,'Prev'));
+// check.addEventListener('change', () => {
+//     console.log(check.checked);
+//     if(check.checked) {
+//         console.log('reda');
+//     }
+// })
